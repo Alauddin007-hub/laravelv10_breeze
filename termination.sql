f396283a384ba100c341dfe55b94a2c811fe8bae
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2024-02-05 06:59:50 +0600

    6565

---
+++ termination.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 05, 2024 at 01:56 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laravel-smarthr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `termination`
+--
+
+CREATE TABLE `termination` (
+  `id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `termination_type` varchar(50) NOT NULL,
+  `subject` varchar(50) DEFAULT NULL,
+  `role_type_id` int(11) NOT NULL,
+  `notice_date` date NOT NULL,
+  `terminated_date` date NOT NULL,
+  `discription` varchar(200) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modified_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
