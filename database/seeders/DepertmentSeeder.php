5f48609464569c0c1798a1b89857b84156128995
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2024-02-28 09:52:25 +0600

    seed

--- database/seeders/DepertmentSeeder.php
+++ database/seeders/DepertmentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DepertmentSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class DepertmentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('departments')->insert([
+            'name' => "HR Department",
+        ]);
+        DB::table('departments')->insert([
+            'name' => "IT Department",
+        ]);
+        DB::table('departments')->insert([
+            'name' => "Management Department",
+        ]);
     }
 }
--- database/seeders/DesignationSeeder.php
+++ database/seeders/DesignationSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DesignationSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class DesignationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('designations')->insert([
+            'name' => "HR Amin",
+            'department_id' => "1",
+        ]);
+        DB::table('designations')->insert([
+            'name' => "Senior Developer",
+            'department_id' => "2",
+        ]);
+        DB::table('designations')->insert([
+            'name' => "Managing Director",
+            'department_id' => "3",
+        ]);
     }
 }
